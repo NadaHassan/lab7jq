e64804b15f5d7759b3eaa02ff925001140be63eb
Nada Hassan <nadahassan013@gmail.com>
2016-04-05 21:08:57 -0400

    lab7 jq added

---
+++ apps.js
@@ -0,0 +1,19 @@
+var home = $('#home'); //,'#about','#contact','#order'
+home.css('width','280px');
+home.css('height','50px');
+
+home.on('hover', function(){
+home.fadeTo('slow',0.25);
+});
+
+var about = $('#about');
+about.css('width','280px');
+about.css('height','50px');
+
+var contact = $('#contact');
+contact.css('width','280px');
+contact.css('height','50px');
+
+var order = $('#order');
+order.css('width','280px');
+order.css('height','50px');
